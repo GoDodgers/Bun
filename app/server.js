1b518a60f8ef5e00eb2dbcf47cb544d95ec1fb18
Alberto Esquivias <alberto.m.esquivias@gmail.com>
2023-09-27 21:49:51 -0700

    serve dynamic tsx and css

--- app/server.js
+++ app/server.js
@@ -1,11 +1,28 @@
+import { file } from "bun";
+
+
 export const server = Bun.serve({
-    port: 2222,
-    fetch(req) {
+    port: Bun.env.PORT || 3000,
+    fetch(req) {  
         
         const url = new URL(req.url);
         console.log(req);
-        
-        return new Response("hello world");
+        console.log(url);
+
+        switch (url.pathname) {
+            case '/': 
+                return new Response(file("./index.html"));
+            case '/dist/index.js':
+                return new Response(file('./dist/index.js'));
+            case '/dist/index-cbc7bef3a51ebccd.css':
+                return new Response(file('./dist/index-cbc7bef3a51ebccd.css'));
+            default:
+                return new Response('404');
+        }
+
     }
 });
+
+console.log(`port :: ${ Bun.env.PORT || 3000 }`);
+// console.log(`hola!!`);
 
---
+++ index.tsx
@@ -0,0 +1,8 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+
+import './index.css';
+
+const root = document.getElementById('root');
+
+console.log(root);
