dc5526f628af03ff966cfec1370f38c484412d45
Alberto Esquivias <alberto.m.esquivias@gmail.com>
2023-10-02 20:27:33 -0700

    css stuff

--- src/app/server.js
+++ src/app/server.js
@@ -7,13 +7,12 @@ export const server = Bun.serve({
     port: Bun.env.PORT || 3000,
     fetch(req) {  
         const url = new URL(req.url);
-        let filePath = "." + url.pathname;
         let dir = import.meta.dir;
         let dist = './dist/';
 
         import.meta.dist = "/Users/berto/Desktop/playground/bun/bun/dist";
         
-        console.log(`dir ::`, dir, '\n');
+        // console.log(`dir ::`, dir, '\n');
 
         console.log(req, '\n');
         console.log(url, '\n');
@@ -27,29 +26,45 @@ export const server = Bun.serve({
         
         if (url.pathname.match(/css$/)) {
             
-            let fName = fs.readdirSync(dist).filter((f) => f.match(/css$/))
-            .reduce((acc, curr) => {
-                if (!acc) {
-                    return curr;
+            let distFiles = fs.readdirSync(dist);
+
+            console.log(`distFiles ::\n`, distFiles);
+
+            let fName = distFiles.filter((f) => {
+                if (f.match(/css$/)) {
+                    if (f.match(/index/) && url.pathname.match(/index/)) {
+                        return true;
+                    }
+                    if (f.match(/main/) && url.pathname.match(/main/)) {
+                        return true;
+                    }
                 }
-                console.log(dist  + acc);
-                console.log(dist  + curr);
+                return false;
+            });
+            
+            console.log(`distFiles Filtered ::\n`, fName);
+
+            fName.reduce((acc, curr) => {
+
+                console.log(`dist  + acc`, dist  + acc);
+                console.log(`dist  + curr`, dist  + curr);
 
                 var time1 = fs.statSync(dist + acc).ctime;
                 var time2 = fs.statSync(dist + curr).ctime;
 
                 if (time1 > time2) {
-                    // fs.unlink(dist + curr);
                     fs.unlink(import.meta.dist + '/' + curr, (err) => {
                         if (err) throw err;
-                        console.log(`${import.meta.dist + '/' + f} was deleted`);
+                        console.log(`${ import.meta.dist + '/' + f } was deleted`);
                     });
                     return acc;
                 }
+
                 fs.unlink(import.meta.dist + '/' + acc, (err) => {
                     if (err) throw err;
-                    console.log(`${import.meta.dist + '/' + f} was deleted`);
+                    console.log(`${ import.meta.dist + '/' + f } was deleted`);
                 });
+
                 return curr;
             });
             
